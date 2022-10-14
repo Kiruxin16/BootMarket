2d1c95460ad77c1e4f5a284065c3e2c1d83cfe74
KpoJIer <kiruxin16@gmail.com>
2022-10-14 15:35:35 +0700

    Initial commit

---
+++ src/main/java/ru/geekbrains/homework/BootMarket/BootMarketApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.homework.BootMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMarketApplication.class, args);
+	}
+
+}
